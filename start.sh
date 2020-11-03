b263d420b9e535a7e819f742e46c26c3ef119b88
xiaotong <cxtbj@cn.ibm.com>
2020-11-03 14:38:28 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "========== list ==========="
+ps -ef
+
+echo "========== service status =========="
+systemctl status sshd
+
 echo "start mongo db"
 /usr/bin/mongod -f /etc/mongod.conf
 
