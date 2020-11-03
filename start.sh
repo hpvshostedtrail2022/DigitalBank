b64399a194094396235a16e2d8f32ba07d2cc7b5
xiaotong <cxtbj@cn.ibm.com>
2020-11-03 14:51:36 +0800

    add output

--- start.sh
+++ start.sh
@@ -2,9 +2,14 @@
 
 echo "========== list ==========="
 ps -ef
+ls -l /bin
+/bin/systemctl status
 
 echo "========== service status =========="
-systemctl status sshd
+/etc/init.d/ssh status
+
+echo "========== service start =========="
+/etc/init.d/ssh start
 
 echo "start mongo db"
 /usr/bin/mongod -f /etc/mongod.conf
