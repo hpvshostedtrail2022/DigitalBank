b8a5d683a8aeb8e9c36e2583bace01fb2458d2a5
xiaotong <cxtbj@cn.ibm.com>
2020-11-03 14:27:12 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 echo "start mongo db"
-/usr/bin/mongod -f /etc/mongod.conf &
+/usr/bin/mongod -f /etc/mongod.conf
 
-echo "======================= list ======================"
-pgrep mongo -l
-
-#echo ""
