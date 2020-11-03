826db1847d2da2dde4c933c8febbc858b20946a4
xiaotong <cxtbj@cn.ibm.com>
2020-11-03 15:06:31 +0800

    update

--- start.sh
+++ start.sh
@@ -3,13 +3,25 @@
 echo "========== list ==========="
 ps -ef
 ls -l /bin
-/bin/systemctl status
+#/bin/systemctl status
+
+echo "========== service status =========="
+/etc/init.d/ssh status
+ll /etc/ssh/
+whoami
+echo "========== service start =========="
+/etc/init.d/ssh start
+echo "========== service status =========="
+/etc/init.d/ssh status
+
+
+rm -r /etc/ssh/ssh*key
+dpkg-reconfigure openssh-server
 
 echo "========== service status =========="
 /etc/init.d/ssh status
 
-echo "========== service start =========="
-/etc/init.d/ssh start
+ls -al /etc/ssh/ssh*key
 
 echo "start mongo db"
 /usr/bin/mongod -f /etc/mongod.conf
